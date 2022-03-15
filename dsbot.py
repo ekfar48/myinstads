b01011fe0dd0df1568dcd51517a40767f99f942f
ekfar <68909674+ekfar48@users.noreply.github.com>
2022-03-15 10:28:32 +0300

    Update dsbot.py

--- dsbot.py
+++ dsbot.py
@@ -28,25 +28,63 @@ async def on_ready():
         activity=discord.Game(">open,move,click")) 
         
 @client.command()
-async def open(ctx,insta="morgen_shtern"):
-    chrome_options = webdriver.ChromeOptions() 
-    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN") 
-    chrome_options.add_argument("--headless") 
-    chrome_options.add_argument("--disable-dev-shm-usage") 
-    chrome_options.add_argument("--no-sandbox") 
-    ##chrome_options.add_argument("--start-maximized") 
-    chrome_options.add_argument('window-size=945,1030') 
-    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options) 
-    driver.get(f"https://www.instagram.com/{insta}") 
-    #driver.maximize_window()
-    ##
-    screenshot = driver.save_screenshot('my_screenshot.png')
-    ##await ctx.send(file=discord.File("my_screenshot.png"))
-
-    embed_en = discord.Embed(title=f"text", description=f"**txtz", color=0xea4335)
-    embed_en.set_image(url="attachment://my_screenshot.png") 
-
-    file = discord.File(r"my_screenshot.png", filename="my_screenshot.png")
-    await ctx.send(file=file,embed=embed_en)
+async def open(ctx,arg1):
+    #подключения драйвера
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--start-maximized")
+    chrome_options.add_argument('window-size=945,1030')
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
     
+    
+    driver.get(f"https://www.google.com/search?q={arg1}+aktie")
+    driver.maximize_window()
+    
+    sleep(1)
+    
+    elem= driver.find_element_by_css_selector(".NprOob")
+    elems= driver.find_element_by_css_selector(".WlRRw")
+    print(elem.text)
+    
+    screenshot = driver.save_screenshot('my_screenshot.png')
+#     await ctx.send(file=discord.File("my_screenshot.png"))
+    
+
+
+    im = Image.open('my_screenshot.png')
+    sleep(1)
+    im_crop = im.crop((10, 345, 690, 805))
+    sleep(1)
+    im_crop.save('guido_pillow_crop.png', quality=95)
+    
+#     await ctx.send(file=discord.File("guido_pillow_crop.png"))
+
+    textil = elems.text
+    lents = len(elems.text)
+    bin = int(lents) - int(5)
+    rel = textil[0:bin]
+    print(textil[0:1])
+
+    if str(textil[0:1]) == str("+"):
+        embed_en = discord.Embed(title=f"▬▬▬▬▬▬▬▬[Акции {arg1}]▬▬▬▬▬▬▬▬", description=f"**Стоимость:** {elem.text} │ **просадок:** {rel}", color=0x3cd126)
+    else:
+        embed_en = discord.Embed(title=f"▬▬▬▬▬▬▬▬[Акции {arg1}]▬▬▬▬▬▬▬▬", description=f"**Стоимость:** {elem.text} │ **просадок:** {rel}", color=0xea4335)
+        
+#     await ctx.send(elem.text)
+#     embed_en = discord.Embed(title=f"▬▬▬▬▬▬▬▬[Акции {arg1}]▬▬▬▬▬▬▬▬", description=f"**Стоимость:** {elem.text} │ **просадок:**fds", color=0xea4335)
+
+
+
+    file = discord.File(r"guido_pillow_crop.png", filename="guido_pillow_crop.png")
+    embed_en.set_image(url="attachment://guido_pillow_crop.png")
+
+    
+    await ctx.send(file=file, embed=embed_en)
+    
+    os.remove("my_screenshot.png")
+    os.remove("guido_pillow_crop.png")
+
 client.run(os.environ['token'])
